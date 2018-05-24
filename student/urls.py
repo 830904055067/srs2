8c2a65bd372c5aa711b81bcd8e5733b3a32a5fc9
830904055067 <mgkamarull@gmail.com>
2018-05-24 15:42:30 +0800

    updated url and view

--- student/urls.py
+++ student/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$',views.home,name='student_home'),
+    url(r'^list_json/$', views.student_list_json.as_view(), name="student_list_json"), #utk papar list student bila guna url .../list_json
+    url(r'^home/$',views.home_json,name='student_home_json')   #utk papar page home
 ]
