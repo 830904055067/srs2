46129db56f1819b72e7fd4f2d983a78aa9209b22
830904055067 <mgkamarull@gmail.com>
2018-05-24 11:11:49 +0800

    my 2nd and 3rd commit

--- srs2/settings.py
+++ srs2/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
-
+# created by kamarull ariffin on thursday 24 mei 2018
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -119,3 +119,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+#commenttest
