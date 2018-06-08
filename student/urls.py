18a160d7bb88746af1c9512d1ebbf861d59b64c7
830904055067 <mgkamarull@gmail.com>
2018-06-08 12:03:06 +0800

    add new page student detail

--- student/urls.py
+++ student/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^list_json/$', views.student_list_json.as_view(), name="student_list_json"), #utk papar list student bila guna url .../list_json
     url(r'^home/$',views.home_json,name='student_home_json'),   #utk papar page home
     url(r'^sbadmin/$',views.home_sbadmin,name='student_home_sbadmin'),   #utk papar page sbadmin
+    url(r'^(?P<pk>\d+)/$', views.student_detail, name='student_detail'),   #utk papar student detail
 ]
